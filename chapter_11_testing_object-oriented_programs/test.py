b87c399649b949f6fd267f21a26fb20504a81abb
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-04-09 19:46:08 -0700

    Add expected fail test that asserts equality between str and float.

--- chapter_11_testing_object-oriented_programs/test.py
+++ chapter_11_testing_object-oriented_programs/test.py
@@ -4,6 +4,9 @@ class CheckNumbers(unittest.TestCase):
     def test_int_float(self):
         self.assertEqual(1, 1.0)
 
+    def test_str_float(self):
+        self.assertEqual("1", 1.0)
+
 if __name__ == "__main__":
     unittest.main()
 
