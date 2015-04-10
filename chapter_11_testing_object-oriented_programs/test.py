86e0312967c56e0782e916b23c57e5fde215d526
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-04-09 20:00:27 -0700

    Add average method and tests to check ZeroDivisionError is raised.

--- chapter_11_testing_object-oriented_programs/test.py
+++ chapter_11_testing_object-oriented_programs/test.py
@@ -1,5 +1,8 @@
 import unittest
 
+def average(seq):
+    return sum(seq) / len(seq)
+
 class CheckNumbers(unittest.TestCase):
     def test_int_float(self):
         self.assertEqual(1, 1.0)
@@ -7,6 +10,13 @@ class CheckNumbers(unittest.TestCase):
     def test_str_float(self):
         self.assertEqual("1", 1.0)
 
+class TestAverage(unittest.TestCase):
+    def test_python30_zero(self):
+        self.assertRaises(ZeroDivisionError, average, [])
+
+    def test_python31_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            average([])
+
 if __name__ == "__main__":
     unittest.main()
-
