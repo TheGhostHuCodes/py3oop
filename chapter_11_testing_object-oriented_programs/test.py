6dba934dfc10fe100f0ea905e55177de94883a38
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-04-09 21:13:58 -0700

    Add decorations to ignore broken tests.

--- chapter_11_testing_object-oriented_programs/test.py
+++ chapter_11_testing_object-oriented_programs/test.py
@@ -7,6 +7,7 @@ class CheckNumbers(unittest.TestCase):
     def test_int_float(self):
         self.assertEqual(1, 1.0)
 
+    @unittest.expectedFailure
     def test_str_float(self):
         self.assertEqual("1", 1.0)
 
