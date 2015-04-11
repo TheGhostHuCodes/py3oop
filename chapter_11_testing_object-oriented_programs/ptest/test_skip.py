74cdd5505d5bc4561d6ab4ffefe99d676bea2408
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-04-11 09:31:19 -0700

    Add test that is skipped if version is <= 3.0 python.

--- chapter_11_testing_object-oriented_programs/ptest/test_skip.py
+++ chapter_11_testing_object-oriented_programs/ptest/test_skip.py
@@ -7,3 +7,7 @@ def test_simple_skip():
 
     fakeos.do_something_fake()
     assert fakeos.did_not_happen
+
+@py.test.mark.skipif("sys.version_info <= (3, 0)")
+def test_python3():
+    assert b"hello".decode() == "hello"
