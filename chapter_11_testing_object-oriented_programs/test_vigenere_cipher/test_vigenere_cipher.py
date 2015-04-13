8b0f0f1dffa54116e1e0394a0a8a9a303e7e365a
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-04-13 11:35:08 -0700

    Add combine_character method and tests.

--- chapter_11_testing_object-oriented_programs/test_vigenere_cipher/test_vigenere_cipher.py
+++ chapter_11_testing_object-oriented_programs/test_vigenere_cipher/test_vigenere_cipher.py
@@ -1,6 +1,21 @@
 from vigenere_cipher import VigenereCipher
+from vigenere_cipher import combine_character
 
 def test_encode():
     cipher = VigenereCipher("TRAIN")
     encoded = cipher.encode("ENCODEDINPYTHON")
     assert encoded == "XECWQXUIVCRKHWA"
+
+def test_encode_spaces():
+    cipher = VigenereCipher("TRAIN")
+    encoded = cipher.encode("ENCODED IN PYTHON")
+    assert encoded == "XECWQXUIVCRKHWA"
+
+def test_encode_lowercase():
+    cipher = VigenereCipher("TRAIN")
+    encoded = cipher.encode("encoded in Python")
+    assert encoded == "XECWQXUIVCRKHWA"
+
+def test_combine_character():
+    assert combine_character("E", "T") == "X"
+    assert combine_character("N", "R") == "E"
--- chapter_11_testing_object-oriented_programs/vigenere_cipher/vigenere_cipher.py
+++ chapter_11_testing_object-oriented_programs/vigenere_cipher/vigenere_cipher.py
@@ -4,3 +4,10 @@ class VigenereCipher:
 
     def encode(self, plain_text):
         return "XECWQXUIVCRKHWA"
+
+def combine_character(plain_text, keyword):
+    plain_text = plain_text.upper()
+    keyword = keyword.upper()
+    plain_index = ord(plain_text) - ord('A')
+    keyword_index = ord(keyword) - ord('A')
+    return chr(ord('A') + (plain_index + keyword_index) % 26)
