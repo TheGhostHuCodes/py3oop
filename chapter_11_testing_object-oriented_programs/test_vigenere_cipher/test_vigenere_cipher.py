3b8844be0a8c65023d3bba5bcdede298c7becf78
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-04-14 15:42:46 -0700

    Refactor VigenereCipher tests to use a funcarg for setup.

--- chapter_11_testing_object-oriented_programs/test_vigenere_cipher/test_vigenere_cipher.py
+++ chapter_11_testing_object-oriented_programs/test_vigenere_cipher/test_vigenere_cipher.py
@@ -2,45 +2,35 @@ from vigenere_cipher import VigenereCipher
 from vigenere_cipher import combine_character
 from vigenere_cipher import separate_character
 
-def test_encode():
-    cipher = VigenereCipher("TRAIN")
-    encoded = cipher.encode("ENCODEDINPYTHON")
-    assert encoded == "XECWQXUIVCRKHWA"
+def pytest_funcarg__valid_cipher(request):
+    return VigenereCipher("TRAIN")
 
-def test_encode_character():
-    cipher = VigenereCipher("TRAIN")
-    encoded = cipher.encode("E")
-    assert encoded == "X"
+def test_encode(valid_cipher):
+    assert valid_cipher.encode("ENCODEDINPYTHON") == "XECWQXUIVCRKHWA"
 
-def test_encode_spaces():
-    cipher = VigenereCipher("TRAIN")
-    encoded = cipher.encode("ENCODED IN PYTHON")
-    assert encoded == "XECWQXUIVCRKHWA"
+def test_encode_character(valid_cipher):
+    assert valid_cipher.encode("E") == "X"
 
-def test_encode_lowercase():
-    cipher = VigenereCipher("TRAIN")
-    encoded = cipher.encode("encoded in Python")
-    assert encoded == "XECWQXUIVCRKHWA"
+def test_encode_spaces(valid_cipher):
+    assert valid_cipher.encode("ENCODED IN PYTHON") == "XECWQXUIVCRKHWA"
+
+def test_encode_lowercase(valid_cipher):
+    cipher = VigenereCipher("TRain")
+    assert cipher.encode("encoded in Python") == "XECWQXUIVCRKHWA"
 
 def test_combine_character():
     assert combine_character("E", "T") == "X"
     assert combine_character("N", "R") == "E"
 
-def test_extend_keyword_to_length_15():
-    cipher = VigenereCipher("TRAIN")
-    extended = cipher.extend_keyword(15)
-    assert extended == "TRAINTRAINTRAIN"
+def test_extend_keyword_to_length_15(valid_cipher):
+    assert valid_cipher.extend_keyword(15) == "TRAINTRAINTRAIN"
 
-def test_extend_keyword_to_length_16():
-    cipher = VigenereCipher("TRAIN")
-    extended = cipher.extend_keyword(16)
-    assert extended == "TRAINTRAINTRAINT"
+def test_extend_keyword_to_length_16(valid_cipher):
+    assert valid_cipher.extend_keyword(16) == "TRAINTRAINTRAINT"
 
 def test_separate_character():
     assert separate_character("X", "T") == "E"
     assert separate_character("E", "R") == "N"
 
-def test_decode():
-    cipher = VigenereCipher("TRAIN")
-    decoded = cipher.decode("XECWQXUIVCRKHWA")
-    assert decoded == "ENCODEDINPYTHON"
+def test_decode(valid_cipher):
+    assert valid_cipher.decode("XECWQXUIVCRKHWA") == "ENCODEDINPYTHON"
