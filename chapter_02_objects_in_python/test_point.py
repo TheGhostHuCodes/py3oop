cc72b047374a4fe2ae4ce362dc19a5cae7c6e4d8
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-04-15 16:50:42 -0600

    Add tests for Point class creation, move, and calculate_distance methods.

--- chapter_02_objects_in_python/test_point.py
+++ chapter_02_objects_in_python/test_point.py
@@ -1,18 +1,37 @@
 from point import Point
 
-def test_point_creation():
-    p1 = Point()
-    p1.x = 5
-    p1.y = 4
-    p2 = Point()
-    p2.x = 3
-    p2.y = 6
-    assert (p1.x, p1.y) == (5, 4)
-    assert (p2.x, p2.y) == (3, 6)
+def test_point_initializes_to_origin():
+    point = Point()
+    assert (point.x, point.y) == (0, 0)
 
-def test_point_reset():
+def test_point_creation():
+    point1 = Point()
+    point1.x = 5
+    point1.y = 4
+    point2 = Point()
+    point2.x = 3
+    point2.y = 6
+    assert (point1.x, point1.y) == (5, 4)
+    assert (point2.x, point2.y) == (3, 6)
+
+def test_point_resets_to_origin():
     point = Point()
     point.x = 42
     point.y = 42
     point.reset()
     assert (point.x, point.y) == (0, 0)
+
+def test_point_moves():
+    point = Point()
+    point.move(5, 0)
+    assert (point.x, point.y) == (5, 0)
+
+def test_point_calculates_distance_between_two_points():
+    point1 = Point(5, 0)
+    point2 = Point()
+    assert point2.calculate_distance(point1) == point1.calculate_distance(point2)
+
+def test_distance_between_same_point_is_zero():
+    point1 = Point(5, 0)
+    assert point1.calculate_distance(point1) == 0.0
+
